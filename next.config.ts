d935c63b0a30e917d1c2a7db7694f27e9d6bec99
klopp78 <klopp78@users.noreply.github.com>
2026-09-04 01:22:17 +0800

    Deploy static site to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  ...(isGitHubPages
+    ? {
+        output: "export",
+        assetPrefix: "./",
+      }
+    : {}),
 };
 
 export default nextConfig;
